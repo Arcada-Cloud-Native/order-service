aab2463a0e1613fedca4348eff11a175ed675f74
stahlfre <stahlfre@arcada.fi>
2021-03-01 14:56:12 +0200

    get orders by userId

--- routes/orders.js
+++ routes/orders.js
@@ -41,9 +41,9 @@ router.get("/user/:userId", (req, res, next) => {
 });
 router.get("/user/:userId/:id", (req, res, next) => {
   const id = req.params.id;
-  const user_id = req.params.user_id;
+  const userId = req.params.userId;
 
-  Order.find({_id: id, user_id: user_id})
+  Order.find({_id: id, userId: userId})
     .exec()
     .then(document => {
       res.status(200).json(document);
